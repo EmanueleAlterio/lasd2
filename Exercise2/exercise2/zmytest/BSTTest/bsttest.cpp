2ffb9017608c90f2aa931bcf8eb9be2fe3de32e0
emanuele <emanu.alterio@gmail.com>
2024-05-11 16:34:00 +0200

    continuo test bst

--- Exercise2/exercise2/zmytest/BSTTest/bsttest.cpp
+++ Exercise2/exercise2/zmytest/BSTTest/bsttest.cpp
@@ -129,10 +129,10 @@ void testBSTInt(unsigned int & testnum, unsigned int & testerr){
         SetAt(loctestnum, loctesterr, vec, true, 5, 22);
 
         lasd::BST<int> bst4(vec);
-        TraversePreOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<int>);
-        TraverseInOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<int>);
-        TraversePostOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<int>);
-        TraverseBreadth(loctestnum, loctesterr, bst3, true, &TraversePrint<int>);
+        TraversePreOrder(loctestnum, loctesterr, bst4, true, &TraversePrint<int>);
+        TraverseInOrder(loctestnum, loctesterr, bst4, true, &TraversePrint<int>);
+        TraversePostOrder(loctestnum, loctesterr, bst4, true, &TraversePrint<int>);
+        TraverseBreadth(loctestnum, loctesterr, bst4, true, &TraversePrint<int>);
 
         EqualBST(loctestnum, loctesterr, bst3, bst4);
 
@@ -161,10 +161,277 @@ void testBSTInt(unsigned int & testnum, unsigned int & testerr){
 }
     
 
+void testBSTDouble(unsigned int & testnum, unsigned int & testerr){
+
+    unsigned int loctestnum = 0, loctesterr = 0;
+    cout << endl << "Begin of BST<double> Test" << endl;
+
+    try
+    {
+        lasd::BST<double> bst;
+        Empty(loctestnum, loctesterr, bst, true);
+        Size(loctestnum, loctesterr, bst, true, 0);
+
+        InsertC(loctestnum, loctesterr, bst, true, 12.1);
+        InsertC(loctestnum, loctesterr, bst, true, 5.3);
+        InsertC(loctestnum, loctesterr, bst, true, 15.2);
+        InsertC(loctestnum, loctesterr, bst, true, 3.9);
+        InsertC(loctestnum, loctesterr, bst, true, 7.7);
+        InsertC(loctestnum, loctesterr, bst, true, 13.5);
+        InsertC(loctestnum, loctesterr, bst, true, 17.1);
+        InsertC(loctestnum, loctesterr, bst, true, 1.4);
+        InsertC(loctestnum, loctesterr, bst, true, 9.1);
+
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<double>);
+        Min(loctestnum, loctesterr, bst, true, 1.4);
+        Max(loctestnum, loctesterr, bst, true, 17.1);
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<double>);
+        InsertC(loctestnum, loctesterr, bst, true, 19.2);
+        InsertC(loctestnum, loctesterr, bst, true, 0.2);
+        Size(loctestnum, loctesterr, bst, true, 11);
+        Min(loctestnum, loctesterr, bst, true, 0.2);
+        Max(loctestnum, loctesterr, bst, true, 19.2);
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<double>);
+        MinNRemove(loctestnum, loctesterr, bst, true, 0.2);
+        MaxNRemove(loctestnum, loctesterr, bst, true, 19.2);
+        Remove(loctestnum, loctesterr, bst, true, 7.7);
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<double>);
+        Exists(loctestnum, loctesterr, bst, false, 7.7);
+        Exists(loctestnum, loctesterr, bst, true, 13.5);
+
+        Successor(loctestnum, loctesterr, bst, false, 13.5, 19.2);
+        Successor(loctestnum, loctesterr, bst, true, 13.5, 15.2);
+        Predecessor(loctestnum, loctesterr, bst, true, 13.5, 12.1);
+        Successor(loctestnum, loctesterr, bst, false, 12.1, 15.2);
+        Successor(loctestnum, loctesterr, bst, true, 12.1, 13.5);
+        Predecessor(loctestnum, loctesterr, bst, true, 12.1, 9.1);
+
+        PredecessorNRemove(loctestnum, loctesterr, bst, true, 12.1, 9.1);
+        SuccessorNRemove(loctestnum, loctesterr, bst, true, 12.1, 13.5);
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<double>);
 
 
+        lasd::BST<double> copbst(bst);
+        EqualBST(loctestnum, loctesterr, bst, copbst);
+
+        Remove(loctestnum, loctesterr, copbst, true, 1.4);
+
+        NonEqualBST(loctestnum, loctesterr, bst, copbst);
+
+        InsertC(loctestnum, loctesterr, copbst, true, 1.4);
+
+        EqualBST(loctestnum, loctesterr, bst, copbst);
+
+        lasd::BST<double> movbst(move(copbst));
+        EqualBST(loctestnum, loctesterr, bst, movbst);
+        NonEqualBST(loctestnum, loctesterr, bst, copbst);
+        Empty(loctestnum, loctesterr, copbst, true);
+        Size(loctestnum, loctesterr, copbst, true, 0);
+
+        lasd::BST<double> bst2 = copbst;
+        Empty(loctestnum, loctesterr, bst2, true);
+        bst2 = movbst;
+        Empty(loctestnum, loctesterr, bst2, false);
+        Size(loctestnum, loctesterr, bst2, true, 6);
+        EqualBST(loctestnum, loctesterr, bst, movbst);
+
+        bst2.Clear();
+        Empty(loctestnum, loctesterr, bst2, true);
+        bst2 = move(movbst);
+        Empty(loctestnum, loctesterr, bst2, false);
+        NonEqualBST(loctestnum, loctesterr, bst2, movbst);
+        TraverseBreadth(loctestnum, loctesterr, bst2, true, &TraversePrint<double>);
+        TraverseBreadth(loctestnum, loctesterr, movbst, true, &TraversePrint<double>);
+
+        lasd::List<double> lst;
+        InsertAtBack(loctestnum, loctesterr, lst, true, 12.3);
+        InsertAtBack(loctestnum, loctesterr, lst, true, 9.9);
+        InsertAtBack(loctestnum, loctesterr, lst, true, 22.1);
+        InsertAtBack(loctestnum, loctesterr, lst, true, 1.5);
+        InsertAtBack(loctestnum, loctesterr, lst, true, 3.3);
+        InsertAtBack(loctestnum, loctesterr, lst, true, 25.4);
+
+        lasd::BST<double> bst3(lst);
+        TraversePreOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<double>);
+        TraverseInOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<double>);
+        TraversePostOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<double>);
+        TraverseBreadth(loctestnum, loctesterr, bst3, true, &TraversePrint<double>);
+
+        lasd::Vector<double> vec(6);
+        SetAt(loctestnum, loctesterr, vec, true, 0, 12.3);
+        SetAt(loctestnum, loctesterr, vec, true, 1, 9.9);
+        SetAt(loctestnum, loctesterr, vec, true, 2, 25.4);
+        SetAt(loctestnum, loctesterr, vec, true, 3, 1.5);
+        SetAt(loctestnum, loctesterr, vec, true, 4, 3.3);
+        SetAt(loctestnum, loctesterr, vec, true, 5, 22.1);
+
+        lasd::BST<double> bst4(vec);
+        TraversePreOrder(loctestnum, loctesterr, bst4, true, &TraversePrint<double>);
+        TraverseInOrder(loctestnum, loctesterr, bst4, true, &TraversePrint<double>);
+        TraversePostOrder(loctestnum, loctesterr, bst4, true, &TraversePrint<double>);
+        TraverseBreadth(loctestnum, loctesterr, bst4, true, &TraversePrint<double>);
+
+        EqualBST(loctestnum, loctesterr, bst3, bst4);
+
+        lasd::BTBreadthIterator<double> itr4(bst4);
+        GetItrValue(loctestnum, loctesterr, itr4, true, 12.3);
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, 9.9);
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, 25.4);
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, 1.5);
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, 22.1);
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, 3.3);
 
 
+    }catch (...) {
+        loctestnum++; loctesterr++;
+        cout << endl << "Unmanaged error! " << endl;
+    }
+
+    cout << endl << "Errori: " << loctesterr  << " Test: " << loctestnum << endl;
+    testnum += loctestnum;
+    testerr += loctesterr;
+}
+
+
+void testBSTString(unsigned int & testnum, unsigned int & testerr){
+
+    unsigned int loctestnum = 0, loctesterr = 0;
+    cout << endl << "Begin of BST<string> Test" << endl;
+
+    try
+    {
+        lasd::BST<string> bst;
+        Empty(loctestnum, loctesterr, bst, true);
+        Size(loctestnum, loctesterr, bst, true, 0);
+
+        InsertC(loctestnum, loctesterr, bst, true, string("B"));
+        InsertC(loctestnum, loctesterr, bst, true, string("C"));
+        InsertC(loctestnum, loctesterr, bst, true, string("D"));
+        InsertC(loctestnum, loctesterr, bst, true, string("E"));
+        InsertC(loctestnum, loctesterr, bst, true, string("F"));
+        InsertC(loctestnum, loctesterr, bst, true, string("G"));
+        InsertC(loctestnum, loctesterr, bst, true, string("H"));
+        InsertC(loctestnum, loctesterr, bst, true, string("I"));
+        InsertC(loctestnum, loctesterr, bst, true, string("L"));
+
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<string>);
+        Min(loctestnum, loctesterr, bst, true, string("B"));
+        Max(loctestnum, loctesterr, bst, true, string("L"));
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<string>);
+        InsertC(loctestnum, loctesterr, bst, true, string("A"));
+        InsertC(loctestnum, loctesterr, bst, true, string("M"));
+        Size(loctestnum, loctesterr, bst, true, 11);
+        Min(loctestnum, loctesterr, bst, true, string("A"));
+        Max(loctestnum, loctesterr, bst, true, string("M"));
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<string>);
+        MinNRemove(loctestnum, loctesterr, bst, true, string("A"));
+        MaxNRemove(loctestnum, loctesterr, bst, true, string("M"));
+        Remove(loctestnum, loctesterr, bst, true, string("F"));
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<string>);
+        Exists(loctestnum, loctesterr, bst, false, string("F"));
+        Exists(loctestnum, loctesterr, bst, true, string("E"));
+
+        Successor(loctestnum, loctesterr, bst, false, string("D"), string("F"));
+        Successor(loctestnum, loctesterr, bst, true, string("D"), string("E"));
+        Predecessor(loctestnum, loctesterr, bst, true, string("D"), string("C"));
+        Successor(loctestnum, loctesterr, bst, false, string("I"), string("I"));
+        Successor(loctestnum, loctesterr, bst, true, string("E"), string("G"));
+        Predecessor(loctestnum, loctesterr, bst, true, string("G"), string("E"));
+
+        PredecessorNRemove(loctestnum, loctesterr, bst, true, string("D"), string("C"));
+        SuccessorNRemove(loctestnum, loctesterr, bst, true, string("D"), string("E"));
+        TraverseBreadth(loctestnum, loctesterr, bst, true, &TraversePrint<string>);
+
+
+        lasd::BST<string> copbst(bst);
+        EqualBST(loctestnum, loctesterr, bst, copbst);
+
+        Remove(loctestnum, loctesterr, copbst, true, string("D"));
+
+        NonEqualBST(loctestnum, loctesterr, bst, copbst);
+
+        InsertC(loctestnum, loctesterr, copbst, true, string("D"));
+
+        EqualBST(loctestnum, loctesterr, bst, copbst);
+
+        lasd::BST<string> movbst(move(copbst));
+        EqualBST(loctestnum, loctesterr, bst, movbst);
+        NonEqualBST(loctestnum, loctesterr, bst, copbst);
+        Empty(loctestnum, loctesterr, copbst, true);
+        Size(loctestnum, loctesterr, copbst, true, 0);
+
+        lasd::BST<string> bst2 = copbst;
+        Empty(loctestnum, loctesterr, bst2, true);
+        bst2 = movbst;
+        Empty(loctestnum, loctesterr, bst2, false);
+        Size(loctestnum, loctesterr, bst2, true, 6);
+        EqualBST(loctestnum, loctesterr, bst, movbst);
+
+        bst2.Clear();
+        Empty(loctestnum, loctesterr, bst2, true);
+        bst2 = move(movbst);
+        Empty(loctestnum, loctesterr, bst2, false);
+        NonEqualBST(loctestnum, loctesterr, bst2, movbst);
+        TraverseBreadth(loctestnum, loctesterr, bst2, true, &TraversePrint<string>);
+        TraverseBreadth(loctestnum, loctesterr, movbst, true, &TraversePrint<string>);
+
+        lasd::List<string> lst;
+        InsertAtBack(loctestnum, loctesterr, lst, true, string("L"));
+        InsertAtBack(loctestnum, loctesterr, lst, true, string("D"));
+        InsertAtBack(loctestnum, loctesterr, lst, true, string("O"));
+        InsertAtBack(loctestnum, loctesterr, lst, true, string("E"));
+        InsertAtBack(loctestnum, loctesterr, lst, true, string("A"));
+        InsertAtBack(loctestnum, loctesterr, lst, true, string("M"));
+
+        lasd::BST<string> bst3(lst);
+        TraversePreOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<string>);
+        TraverseInOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<string>);
+        TraversePostOrder(loctestnum, loctesterr, bst3, true, &TraversePrint<string>);
+        TraverseBreadth(loctestnum, loctesterr, bst3, true, &TraversePrint<string>);
+
+        lasd::Vector<string> vec(6);
+        SetAt(loctestnum, loctesterr, vec, true, 0, string("D"));
+        SetAt(loctestnum, loctesterr, vec, true, 1, string("M"));
+        SetAt(loctestnum, loctesterr, vec, true, 2, string("O"));
+        SetAt(loctestnum, loctesterr, vec, true, 3, string("E"));
+        SetAt(loctestnum, loctesterr, vec, true, 4, string("A"));
+        SetAt(loctestnum, loctesterr, vec, true, 5, string("L"));
+
+        lasd::BST<string> bst4(vec);
+        TraversePreOrder(loctestnum, loctesterr, bst4, true, &TraversePrint<string>);
+        TraverseInOrder(loctestnum, loctesterr, bst4, true, &TraversePrint<string>);
+        TraversePostOrder(loctestnum, loctesterr, bst4, true, &TraversePrint<string>);
+        TraverseBreadth(loctestnum, loctesterr, bst4, true, &TraversePrint<string>);
+
+        EqualBST(loctestnum, loctesterr, bst3, bst4);
+
+        lasd::BTBreadthIterator<string> itr4(bst4);
+        GetItrValue(loctestnum, loctesterr, itr4, true, string("D"));
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, string("A"));
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, string("M"));
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, string("E"));
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, string("O"));
+        ++itr4;
+        GetItrValue(loctestnum, loctesterr, itr4, true, string("L"));
+
+    }catch (...) {
+        loctestnum++; loctesterr++;
+        cout << endl << "Unmanaged error! " << endl;
+    }
+
+    cout << endl << "Errori: " << loctesterr  << " Test: " << loctestnum << endl;
+    testnum += loctestnum;
+    testerr += loctesterr;
+}
 
 
 
@@ -176,6 +443,9 @@ void BSTTest(unsigned int & testnum, unsigned int & testerr){
     cout << "-------------------------------------------------------------------------------" << endl;
 
     testBSTInt(loctestnum, loctesterr);
+    testBSTDouble(loctestnum, loctesterr);
+    testBSTString(loctestnum, loctesterr);
+
 
     cout << "-------------------------------------------------------------------------------" << endl;
     testnum += loctestnum;
