f06027412e94d560e755831c20a6bc4b0af536f6
emanuele <emanu.alterio@gmail.com>
2024-05-17 10:10:38 +0200

    aggiornamenti codice

--- Exercise2/exercise2/zmytest/test.cpp
+++ Exercise2/exercise2/zmytest/test.cpp
@@ -1,6 +1,4 @@
 
-#include "./BinaryTreeTest/bttest.hpp"
-#include "./BSTTest/bsttest.hpp"
 
 /* ************************************************************************** */
 
@@ -10,18 +8,4 @@ using namespace std;
 
 /* ************************************************************************** */
 
-void mytest() {
-  cout << endl << "*************Inizio My Test*************" << endl;
-
-  unsigned int loctestnum, loctesterr;
-  unsigned int testnum = 0, testerr = 0;
-  
-  loctestnum = 0; loctesterr = 0;
-  BinaryTreeTest(loctestnum, loctesterr);
-  BSTTest(loctestnum, loctesterr);
-
-  testnum += loctestnum; testerr += loctesterr;
-
-  cout << endl << "*************Fine My Test*************";
-  cout << endl << "Errori: " << testerr << " Test: " << testnum << endl;
-}
+void mytest() {}
